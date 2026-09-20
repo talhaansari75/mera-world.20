b4e71d313395e00930c9123cf8daced6f7a00962
talhaansari75 <talhaansari75@gmail.com>
2026-09-20 23:34:12 +0500

    fix: disable auth gate and set VITE_AUTH_ENABLED false

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,18 +2,15 @@ import { createFileRoute } from "@tanstack/react-router";
 import { GameApp } from "@/components/app/GameApp";
 import { GameErrorBoundary } from "@/components/app/GameErrorBoundary";
 import { AgreementGate } from "@/components/app/AgreementGate";
-import { RedirectToSignIn, SignInGate } from "@/lib/auth/gates";
 
 export const Route = createFileRoute("/")({ component: Home });
 
 function Home() {
   return (
     <GameErrorBoundary>
-      <SignInGate fallback={<RedirectToSignIn />}>
-        <AgreementGate>
-          <GameApp />
-        </AgreementGate>
-      </SignInGate>
+      <AgreementGate>
+        <GameApp />
+      </AgreementGate>
     </GameErrorBoundary>
   );
 }
