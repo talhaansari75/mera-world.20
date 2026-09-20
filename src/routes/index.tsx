0b2fa94f3625dba1d42f99da4022d6cc140e19f7
talhaansari75 <talhaansari75@gmail.com>
2026-09-20 23:37:31 +0500

    fix: bypass AgreementGate for testing

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,16 +1,13 @@
 import { createFileRoute } from "@tanstack/react-router";
 import { GameApp } from "@/components/app/GameApp";
 import { GameErrorBoundary } from "@/components/app/GameErrorBoundary";
-import { AgreementGate } from "@/components/app/AgreementGate";
 
 export const Route = createFileRoute("/")({ component: Home });
 
 function Home() {
   return (
     <GameErrorBoundary>
-      <AgreementGate>
-        <GameApp />
-      </AgreementGate>
+      <GameApp />
     </GameErrorBoundary>
   );
 }
