e8e889228cbeef36e629510e55729b7ff9253704
talhaansari75 <talhaansari75@gmail.com>
2026-09-21 16:36:16 +0500

    fix: add Better Auth username schema safely

--- migrations/0004_username.sql
+++ migrations/0004_username.sql
@@ -1,9 +1,3 @@
-alter table "user"
-  add column if not exists username text;
-
-alter table "user"
-  add column if not exists "displayUsername" text;
-
-create unique index if not exists user_username_unique
-  on "user"(username)
-  where username is not null;
+alter table "user" add column if not exists "username" text;
+alter table "user" add column if not exists "displayUsername" text;
+create unique index if not exists "user_username_idx" on "user" ("username") where "username" is not null;
