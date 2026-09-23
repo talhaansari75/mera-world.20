5b06befcf643a63a4b0bd05670ababbc9df3d81b
talhaansari75 <talhaansari75@gmail.com>
2026-09-23 08:19:32 +0500

    test: remove stale orphaned v4/v5 test contracts

--- tests/v5/03-roomProtocol.test.ts
+++
@@ -1,4 +0,0 @@
-import test from "node:test";
-import assert from "node:assert/strict";
-import * as m from "../../src/lib/v5/multiplayer/roomProtocol.ts";
-test("v5 multiplayer/roomProtocol exports",()=>{assert.ok(Object.keys(m).length>0);});
